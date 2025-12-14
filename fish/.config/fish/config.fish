584f4083043e8236c71c3a2a0ebe1e78914b1d3f
netanel <netanelbi1991@gmail.com>
2025-12-14 23:02:37 +0200

    Simplify direnv integration in fish config
    Replace custom direnv hook functions with the standard direnv hook command for cleaner and more maintainable setup.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -58,24 +58,5 @@ alias nano=micro
 alias rspeedtest="ssh root@192.168.1.1 'speedtest'"
 
 # Direnv integration
-function __direnv_export_eval --on-event fish_prompt
-    "/usr/bin/direnv" export fish | source
-    if test "$direnv_fish_mode" != "disable_arrow"
-        function __direnv_cd_hook --on-variable PWD
-            if test "$direnv_fish_mode" = "eval_after_arrow"
-                set -g __direnv_export_again 0
-            else
-                "/usr/bin/direnv" export fish | source
-            end
-        end
-    end
-end
+direnv hook fish | source
 
-function __direnv_export_eval_2 --on-event fish_preexec
-    if set -q __direnv_export_again
-        set -e __direnv_export_again
-        "/usr/bin/direnv" export fish | source
-        echo
-    end
-    functions --erase __direnv_cd_hook
-end
